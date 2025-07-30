08f631054c72f3cd289f493d3c30f335f7f20684
SarucamJ <jamessarucam517@gmail.com>
2025-07-30 14:37:59 +0800

    activity3

---
+++ src/bsit/pkg2a/obinuqe/ACTIVITY3.java
@@ -0,0 +1,12 @@
+package bsit.pkg2a.obinuqe;
+
+
+public class ACTIVITY3 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hewo World! :3");
+        
+    }
+    
+}
